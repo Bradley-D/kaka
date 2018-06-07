64b7d34eec14666768c14b3a7e4b2db9dbe5041f
Bradley Davis <Bradley-D@users.noreply.github.com>
2018-06-07 16:41:22 +1000

    [ ADDED ] basic container to give some structure

--- page.php
+++ page.php
@@ -15,7 +15,7 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="content-area col span_8 clr">
 		<main id="main" class="site-main">
 
 		<?php
--- sidebar.php
+++ sidebar.php
@@ -12,6 +12,6 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 }
 ?>
 
-<aside id="secondary" class="widget-area">
+<aside id="secondary" class="widget-area col span_4 clr">
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </aside><!-- #secondary -->
