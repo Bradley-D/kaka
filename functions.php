07c1a74a5c1b9dd4351c0edb7682e60636f6ea64
Bradley Davis <Bradley-D@users.noreply.github.com>
2018-06-12 14:02:44 +1000

    [ ADDED ] feature images

--- functions.php
+++ functions.php
@@ -155,7 +155,7 @@ require trailingslashit( get_template_directory() ) . 'inc/customizer.php';
 /**
  * Load Kaka theme functions.
  */
- require trailingslashit( get_template_directory() ) . 'inc/theme-functions.php';
+ require trailingslashit( get_template_directory() ) . 'inc/kaka-theme-functions.php';
 
 /**
  * Load Jetpack compatibility file.
