d1472b81e3cccbee3658f0cccfd6fe85e1016212
Bradley Davis <Bradley-D@users.noreply.github.com>
2018-06-13 17:20:21 +1000

    [ UPDATE ] mobile menu

--- header.php
+++ header.php
@@ -49,7 +49,7 @@
 				<?php endif; ?>
 			</div><!-- .site-branding -->
 			<nav id="site-navigation" class="main-navigation col span_8 clr">
-				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'kaka' ); ?></button>
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><span class="hamburger-text"><?php esc_html_e( 'Menu', 'kaka' ); ?></span><span class="hamburger"></span></button>
 				<?php
 				wp_nav_menu( array(
 					'theme_location' => 'menu-1',
