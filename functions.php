6e89559a2229ce4e197754d738e281e24c45f3b6
Bradley Davis <Bradley-D@users.noreply.github.com>
2018-06-19 17:21:13 +1000

    [ ADDED ] stylesheet of theme

--- functions.php
+++ functions.php
@@ -123,7 +123,7 @@ add_action( 'widgets_init', 'kaka_widgets_init' );
  */
 function kaka_scripts_styles() {
 	wp_enqueue_style( 'kaka-min-style', trailingslashit( get_template_directory_uri() ) . 'css/style.min.css' );
-	wp_enqueue_style( 'kaka-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'kaka-style', get_stylesheet_uri() array(), '1.0', 'all' );
 	wp_enqueue_script( 'kaka-navigation', trailingslashit( get_template_directory_uri() ) . 'js/navigation.js', array(), '20151215', true );
 	wp_enqueue_script( 'kaka-theme', trailingslashit( get_template_directory_uri() ) . 'js/theme.js', array( 'jquery' ), '20180613', false  );
 	wp_enqueue_script( 'kaka-skip-link-focus-fix', trailingslashit( get_template_directory_uri() ) . 'js/skip-link-focus-fix.js', array(), '20151215', true );
