d14498d1947475932f1d8eeb234dad279e8655be
Bradley Davis <Bradley-D@users.noreply.github.com>
2018-06-22 18:54:28 +1000

    [ ISSUE #16 ] header tag simplify

--- header.php
+++ header.php
@@ -33,15 +33,13 @@
 			<div class="site-branding col span_4 clr">
 				<?php
 				the_custom_logo();
-				if ( is_front_page() && is_home() ) :
-					?>
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<?php
-				else :
-					?>
-					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-					<?php
-				endif;
+				$kaka_header_tag = ( is_front_page() ? 'h1' : 'p'); ?>
+				<<?php echo esc_attr ($kaka_header_tag ); ?> class="site-title">
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+						<?php bloginfo( 'name' ); ?>
+					</a>
+				</<?php echo esc_attr ($kaka_header_tag ); ?> > <?php
+
 				$kaka_description = get_bloginfo( 'description', 'display' );
 				if ( $kaka_description || is_customize_preview() ) :
 					?>
